3c20d8993108bf0d44b2fa914eaa12bb9b689d88
Oleksandr <bounceit1221@gmail.com>
2022-08-21 23:31:33 +0300

    Create icon_camera.dart

---
+++ lib/widgets/buttons/icon_camera.dart
@@ -0,0 +1,56 @@
+import 'package:audio_fairy_tales/recursec/app_icons.dart';
+import 'package:flutter/material.dart';
+
+class IconCamera extends StatelessWidget {
+  IconCamera(
+      {Key? key,
+      required this.onTap,
+      required this.color,
+      required this.colorBorder,
+      required this.position})
+      : super(key: key);
+  final onTap;
+  final Color color;
+  final Color colorBorder;
+  final double position;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Center(
+          child: Container(
+              width: 200.0,
+              height: 200.0,
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(20.0),
+                ),
+              ),
+              child: Image.asset(
+                AppIcons.camera,
+                color: colorBorder,
+              )),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: position),
+          child: Center(
+            child: GestureDetector(
+              onTap: onTap,
+              child: Container(
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  border: Border.all(color: colorBorder),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(50.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
