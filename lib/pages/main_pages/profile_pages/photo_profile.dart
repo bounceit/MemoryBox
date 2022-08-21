1f59fe945df59b1dddbcc5fd22f078c54c099b82
Oleksandr <bounceit1221@gmail.com>
2022-08-21 23:30:27 +0300

    Create photo_profile.dart

---
+++ lib/pages/main_pages/profile_pages/photo_profile.dart
@@ -0,0 +1,47 @@
+import 'package:audio_fairy_tales/pages/main_pages/profile_pages/profile_model.dart';
+import 'package:audio_fairy_tales/utils/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PhotoProfileProfile extends StatelessWidget {
+  const PhotoProfileProfile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String? _image = context.watch<DataModel>().getUserImage!;
+    return Column(
+      children: [
+        Text(
+          'Твоя частичка',
+          style: threeTitleTextStyle,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 35.0),
+          child: Align(
+            alignment: Alignment.center,
+            child: Stack(
+              children: [
+                SizedBox(
+                    width: 200.0,
+                    height: 200.0,
+                    child: ClipRRect(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                        child: _image == 'assets/images/profile_avatar.png'
+                            ? Image.asset(
+                                _image,
+                                fit: BoxFit.cover,
+                              )
+                            : Image.network(
+                                context.watch<DataModel>().getUserImage!,
+                                fit: BoxFit.cover,
+                              ))),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
