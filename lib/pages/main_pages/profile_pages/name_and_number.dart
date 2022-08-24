41766ef1b60716ed17e2c8391e5d3570641f5f03
Oleksandr <bounceit1221@gmail.com>
2022-08-24 15:13:35 +0300

    Update name_and_number.dart

--- lib/pages/main_pages/profile_pages/name_and_number.dart
+++ lib/pages/main_pages/profile_pages/name_and_number.dart
@@ -2,6 +2,7 @@ import 'package:audio_fairy_tales/pages/main_pages/profile_pages/profile_model.d
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../recursec/app_colors.dart';
 import '../../../utils/constants.dart';
 import '../../../widgets/container_shadow.dart';
 
@@ -24,13 +25,26 @@ class NameAndNumber extends StatelessWidget {
         const SizedBox(
           height: 10.0,
         ),
-        ContainerShadow(
-          image: const Text(''),
-          width: 0.0,
+        Container(
+          width: 350.0,
           height: 60.0,
-          radius: 50.0,
-          widget: Text(
-            '${context.watch<DataModel>().getNumber}',
+          decoration: BoxDecoration(
+            color: AppColors.white100,
+            borderRadius: BorderRadius.circular(100),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset.fromDirection(1),
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 5,
+              ),
+            ],
+          ),
+          child: Center(
+            child: Text(
+              '${context.watch<DataModel>().getNumber}',
+              style: linkColorText,
+            ),
           ),
         ),
         const SizedBox(
