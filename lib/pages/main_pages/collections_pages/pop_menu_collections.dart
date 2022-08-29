a659683e5c0f413d67307eb42161d96908f54e56
Oleksandr <bounceit1221@gmail.com>
2022-08-29 12:49:57 +0300

    Create pop_menu_collections.dart

---
+++ lib/pages/main_pages/collections_pages/pop_menu_collections.dart
@@ -0,0 +1,19 @@
+
+import 'package:audio_fairy_tales/recursec/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class PopupMenuCollectionPage extends StatelessWidget {
+  const PopupMenuCollectionPage({Key? key}) : super(key: key);
+  // final List<String> _idCollectionsList = [];
+  // final List<String> _idAudioList = [];
+  // final List<PopupMenuEntry> _nameList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    // final state = context.watch<CollectionModel>().getItemDone;
+    return const Icon(
+      Icons.more_horiz,
+      color: AppColors.white,
+    );
+  }
+}
