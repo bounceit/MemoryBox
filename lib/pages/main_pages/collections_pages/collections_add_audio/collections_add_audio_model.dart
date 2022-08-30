a471df26ca7fff32204f6e0ad0c0251512879a67
Oleksandr <bounceit1221@gmail.com>
2022-08-30 22:19:08 +0300

    Create collections_add_audio_model.dart

---
+++ lib/pages/main_pages/collections_pages/collections_add_audio/collections_add_audio_model.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CollectionsAddAudioModel extends ChangeNotifier {
+  String? _searchtxt;
+  String? _searchAddAudio;
+  bool? _done;
+
+  get getDone => _done;
+  void setDone(bool done) {
+    _done = done;
+    notifyListeners();
+  }
+
+  String get getSearchtxt => _searchtxt!;
+
+  void setSearchtxt(String searchtxt) {
+    _searchtxt = _searchtxt;
+    notifyListeners();
+  }
+
+  get getSearchAddAudio => _searchAddAudio;
+
+  void setSearchAddAudio(String searchAddAudio) {
+    _searchAddAudio = searchAddAudio;
+    notifyListeners();
+  }
+}
