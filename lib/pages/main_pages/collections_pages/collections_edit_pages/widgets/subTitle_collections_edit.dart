eef3775235ec85f8690db0620402fe1b29d9a09d
Oleksandr <bounceit1221@gmail.com>
2022-08-31 13:39:29 +0300

    Create subTitle_collections_edit.dart

---
+++ lib/pages/main_pages/collections_pages/collections_edit_pages/widgets/subTitle_collections_edit.dart
@@ -0,0 +1,90 @@
+import 'package:audio_fairy_tales/pages/main_pages/collections_pages/collections_edit_pages/collections_edit_model.dart';
+import 'package:audio_fairy_tales/pages/main_pages/collections_pages/collections_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../../utils/constants.dart';
+import '../collections_edit_model.dart';
+
+class SubTitleCollectionEdit extends StatelessWidget {
+  const SubTitleCollectionEdit({
+    Key? key,
+    required this.idCollection,
+    required this.titleCollection,
+    required this.subTitleCollection,
+    required this.imageCollection,
+  }) : super(key: key);
+  final String idCollection;
+  final String titleCollection;
+  final String subTitleCollection;
+  final String imageCollection;
+
+  void _updateCollection(BuildContext context) {
+    CollectionsRepositories().updateCollection(
+      idCollection,
+      Provider.of<CollectionsEditModel>(context, listen: false).getTitle ??
+          titleCollection,
+      Provider.of<CollectionsEditModel>(context, listen: false).getSubTitle ??
+          subTitleCollection,
+      Provider.of<CollectionsEditModel>(context, listen: false).getImage ??
+          imageCollection,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 15.0,
+        ),
+        const Padding(
+          padding: EdgeInsets.only(
+            left: 28.0,
+          ),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Введите описание...',
+              style: twoBodyTextStyle,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: TextField(
+            maxLines: 3,
+            onChanged: (subTitle) {
+              context.read<CollectionsEditModel>().userSubTitle(subTitle);
+            },
+            style: const TextStyle(
+              fontSize: 16.0,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            right: 10.0,
+          ),
+          child: Align(
+            alignment: Alignment.topRight,
+            child: TextButton(
+              onPressed: () {
+                _updateCollection(context);
+
+                Navigator.pop(context);
+              },
+              child: const Text(
+                'Готово',
+                style: linkColorText,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
