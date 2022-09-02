0ae3f1d9c55b60852cf07e013a4d9de50258f943
Oleksandr <bounceit1221@gmail.com>
2022-09-02 09:33:33 +0300

    Create subscription_page.dart

---
+++ lib/pages/drawer_pages/subscriptions_page/subscription_page.dart
@@ -0,0 +1,54 @@
+import 'package:audio_fairy_tales/pages/drawer_pages/subscriptions_page/widgets/appbar_subscriptions.dart';
+import 'package:audio_fairy_tales/pages/drawer_pages/subscriptions_page/widgets/subscription_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../../../recursec/app_colors.dart';
+import '../../../utils/constants.dart';
+
+class SubscriptionPage extends StatelessWidget {
+  const SubscriptionPage({Key? key}) : super(key: key);
+  static const routeName = '/subscription_page';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.colorAppbar,
+        leading: IconButton(
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+          icon: const Icon(Icons.menu),
+        ),
+        title: const Text(
+          'Подписка',
+          style: twoTitleTextStyle,
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Stack(
+              children: [
+                const AppbarMenuSubscription(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Расширь возможности',
+                      style: twoTitleTextStyle,
+                    ),
+                  ],
+                ),
+                SubscriptionAuthorisation()
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
