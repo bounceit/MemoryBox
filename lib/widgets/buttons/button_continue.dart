79175765f9c5000f9a22c1c98c0eb644bc010d60
Oleksandr <bounceit1221@gmail.com>
2022-09-03 09:32:58 +0300

    Create button_continue.dart

---
+++ lib/widgets/buttons/button_continue.dart
@@ -0,0 +1,32 @@
+import 'package:audio_fairy_tales/recursec/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class ButtonContinue extends StatelessWidget {
+  const ButtonContinue({Key? key, this.onPressed, required this.text})
+      : super(key: key);
+  final String text;
+  final onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ButtonStyle(
+        minimumSize: MaterialStateProperty.all(
+          const Size(275.0, 50.0),
+        ),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+        ),
+        backgroundColor: MaterialStateProperty.all(
+          AppColors.yellow100,
+        ),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 18.0, color: AppColors.white100),
+      ),
+    );
+  }
+}
