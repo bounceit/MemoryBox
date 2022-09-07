129f4ec5c4f53f86cb768ae6860d28dac9abd3bd
Oleksandr <bounceit1221@gmail.com>
2022-09-07 16:11:56 +0300

    Update icon_delete_audio.dart

--- lib/pages/drawer_pages/audio_delete_pages/widgets/icon_delete_audio.dart
+++ lib/pages/drawer_pages/audio_delete_pages/widgets/icon_delete_audio.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 import '../../../../recursec/app_icons.dart';
-import '../../../main_pages/collections_pages/collections_repository.dart';
+import '../../../../repositories/collections_repositories.dart';
+import '../../../../repositories/user_repositories.dart';
 
 class IconDeleteAudio extends StatelessWidget {
-  const IconDeleteAudio({Key? key, required this.idAudio, required this.size})
-      : super(key: key);
+  const IconDeleteAudio({
+    Key? key,
+    required this.idAudio,
+    required this.size,
+  }) : super(key: key);
   final String idAudio;
   final int? size;
   @override
@@ -13,18 +17,20 @@ class IconDeleteAudio extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(15.0),
       child: GestureDetector(
-          onTap: () async {
-            await CollectionsRepositories()
-                .deleteCollectionApp(idAudio, 'DeleteCollections');
-          },
-          child: SizedBox(
-            width: 25.0,
-            height: 25.0,
-            child: Image.asset(
-              AppIcons.recDelete,
-              fit: BoxFit.fill,
-            ),
-          )),
+        onTap: () async {
+          // await UserRepositories.instance.updateSizeRepositories(-size!);
+          await CollectionsRepositories.instance
+              .deleteCollectionApp(idAudio, 'DeleteCollections');
+        },
+        child: SizedBox(
+          width: 25.0,
+          height: 25.0,
+          child: Image.asset(
+            AppIcons.recDelete,
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
     );
   }
 }
