feae468ac0cf45f51f06fff64b93e1ae7dd3b634
Oleksandr <bounceit1221@gmail.com>
2022-09-07 16:11:13 +0300

    Create navigation_state.dart

---
+++ lib/blocs/bloc_navigations/navigation_state.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../../pages/home_pages/home_page.dart';
+
+enum NavigationStateStatus {
+  initial,
+  menu,
+  tab,
+}
+
+@immutable
+class NavigationState {
+  const NavigationState({
+    this.status = NavigationStateStatus.initial,
+    this.currentIndex = 0,
+    this.route = HomePage.routeName,
+  });
+
+  final NavigationStateStatus status;
+  final int currentIndex;
+  final String route;
+
+  NavigationState copyWith({
+    NavigationStateStatus? status,
+    int? currentIndex,
+    String? route,
+  }) {
+    return NavigationState(
+      status: status ?? this.status,
+      currentIndex: currentIndex ?? this.currentIndex,
+      route: route ?? this.route,
+    );
+  }
+}
