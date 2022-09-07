19f3ff842b4274021d273ebfd6dcda2254882b86
Oleksandr <bounceit1221@gmail.com>
2022-09-07 16:11:40 +0300

    Update last_register_page.dart

--- lib/pages/auth_pages/last_register_page.dart
+++ lib/pages/auth_pages/last_register_page.dart
@@ -3,46 +3,70 @@ import 'dart:async';
 import 'package:audio_fairy_tales/pages/auth_pages/regular_user_page.dart';
 import 'package:audio_fairy_tales/recursec/app_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 import '../../repositories/user_repositories.dart';
+import '../../utils/constants.dart';
+import '../../widgets/container_shadow.dart';
+import '../../widgets/uncategorized/app_bar_auth.dart';
 import '../../widgets/uncategorized/main_register_clip_path.dart';
 import '../main_pages/main_of_main.dart';
 
 class LastPageRegister extends StatefulWidget {
   const LastPageRegister({Key? key}) : super(key: key);
-  static const routeName = '/last_register_page';
+  static const routeName = '/last_authorization_page';
 
   @override
-  State<LastPageRegister> createState() => _LastPageRegisterState();
+  State<LastPageRegister> createState() => _LastAuthorizationPageState();
 }
 
-class _LastPageRegisterState extends State<LastPageRegister> {
-  final UserRepositories rep = UserRepositories();
+class _LastAuthorizationPageState extends State<LastPageRegister> {
+  final bool shouldPop = false;
   @override
   void initState() {
-    rep.readUser();
-    rep.firstAuthorization();
-    _setInitialData();
-    super.initState();
-  }
-
-  void _setInitialData() {
-    Timer(const Duration(milliseconds: 5000), () {
-      Navigator.of(context)
-          .pushNamedAndRemoveUntil(Main.routeName, (route) => false);
+    Timer(const Duration(seconds: 5), () {
+      Phoenix.rebirth(context);
     });
+
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-      children: [
-        const RegisterClipPath(),
-        const SizedBox(height: 50),
-        const InitialBoxWidget(),
-        const SizedBox(height: 40),
-        Image.asset(AppIcons.heart),
-      ],
-    ));
+    return WillPopScope(
+      onWillPop: () async {
+        return shouldPop;
+      },
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const AppbarHeaderAuthorization(
+                title: 'Ты супер!',
+                subtitle: '',
+              ),
+              const SizedBox(
+                height: 40.0,
+              ),
+              const ContainerShadow(
+                image: Text(''),
+                width: 300.0,
+                height: 80.0,
+                widget: Text(
+                  'Мы рады тебя видеть',
+                  style: bodyTextStyle,
+                ),
+                radius: 20.0,
+              ),
+              const SizedBox(
+                height: 50.0,
+              ),
+              Image.asset(
+                AppIcons.heart,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
