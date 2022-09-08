17b1e7b3eb9dbb136e1562e24f4172ed7dc7f477
Oleksandr <bounceit1221@gmail.com>
2022-09-08 13:22:57 +0300

    Update popmenu_delete_page.dart

--- lib/pages/drawer_pages/audio_delete_pages/widgets/popmenu_delete_page.dart
+++ lib/pages/drawer_pages/audio_delete_pages/widgets/popmenu_delete_page.dart
@@ -1,18 +1,19 @@
-import 'package:audio_fairy_tales/pages/main_pages/collections_pages/collections_repository.dart';
 import 'package:audio_fairy_tales/recursec/app_colors.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:collection/collection.dart' show IterableZip;
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
+import '../../../../repositories/auth_repository.dart';
+import '../../../../repositories/collections_repositories.dart';
 import '../../../../repositories/user_repositories.dart';
 import '../../../../widgets/buttons/popup_menu_item.dart';
 import '../audio_delete_model.dart';
+import '../bloc/delete_audio_cubbit.dart';
 
 class PopupMenuDeletePage extends StatelessWidget {
   PopupMenuDeletePage({Key? key}) : super(key: key);
-  final CollectionsRepositories _rep = CollectionsRepositories();
-  final UserRepositories repositoriesUser = UserRepositories();
   final List<String> _idAudioList = [];
   final List<int> _sizeList = [];
   final List<String> _idAudioListAll = [];
@@ -20,10 +21,15 @@ class PopupMenuDeletePage extends StatelessWidget {
 
   Future<void> _getIdCollection(BuildContext context) async {
     await FirebaseFirestore.instance
-        .collection(_rep.user!.phoneNumber!)
+        .collection(
+          AuthRepositories.instance.user!.phoneNumber!,
+        )
         .doc('id')
         .collection('DeleteCollections')
-        .where('done', isEqualTo: true)
+        .where(
+          'done',
+          isEqualTo: true,
+        )
         .get()
         .then((querySnapshot) {
       for (var result in querySnapshot.docs) {
@@ -37,7 +43,9 @@ class PopupMenuDeletePage extends StatelessWidget {
 
   Future<void> _getIdCollectionAll(BuildContext context) async {
     await FirebaseFirestore.instance
-        .collection(_rep.user!.phoneNumber!)
+        .collection(
+          AuthRepositories.instance.user!.phoneNumber!,
+        )
         .doc('id')
         .collection('DeleteCollections')
         .get()
@@ -53,97 +61,125 @@ class PopupMenuDeletePage extends StatelessWidget {
 
   Future<void> _delete(BuildContext context) async {
     await _getIdCollection(context);
-    for (var item in IterableZip([_idAudioList, _sizeList])) {
+    for (var item in IterableZip(
+      [
+        _idAudioList,
+        _sizeList,
+      ],
+    )) {
       final idAudio = item[0];
-      // final sizeTemp = item[1];
-      // final size = sizeTemp as int;
-      await _rep.deleteCollectionApp('$idAudio', 'DeleteCollections');
-      // await repositoriesUser.updateSizeRepositories(-size);
+      final sizeTemp = item[1];
+      final size = sizeTemp as int;
+      await CollectionsRepositories.instance.deleteCollectionApp(
+        '$idAudio',
+        'DeleteCollections',
+      );
+      await UserRepositories.instance.updateSizeRepositories(-size);
     }
-    await UserRepositories().updateTotalTimeQuality();
+    await UserRepositories.instance.updateTotalTimeQuality();
+    context.read<DeleteItemDoneCubit>().itemDone();
   }
 
   Future<void> _reestablish(BuildContext context) async {
     await _getIdCollection(context);
     for (var item in _idAudioList) {
-      await _rep.copyPastCollections(
+      await CollectionsRepositories.instance.copyPastCollections(
         item,
         'DeleteCollections',
         'Collections',
       );
-      await _rep.deleteCollection(item, 'DeleteCollections');
-      await UserRepositories().updateTotalTimeQuality();
+      await CollectionsRepositories.instance.deleteCollection(
+        item,
+        'DeleteCollections',
+      );
+      await UserRepositories.instance.updateTotalTimeQuality();
     }
+    context.read<DeleteItemDoneCubit>().itemDone();
   }
 
   Future<void> _deleteAll(BuildContext context) async {
     await _getIdCollectionAll(context);
-    for (var item in IterableZip([_idAudioListAll, _sizeListAll])) {
+    for (var item in IterableZip(
+      [
+        _idAudioListAll,
+        _sizeListAll,
+      ],
+    )) {
       final idAudio = item[0];
       final sizeTemp = item[1];
       final size = sizeTemp as int;
-      await _rep.deleteCollectionApp('$idAudio', 'DeleteCollections');
-      // await repositoriesUser.updateSizeRepositories(-size);
+      await CollectionsRepositories.instance.deleteCollectionApp(
+        '$idAudio',
+        'DeleteCollections',
+      );
+      await UserRepositories.instance.updateSizeRepositories(-size);
     }
 
-    await UserRepositories().updateTotalTimeQuality();
+    await UserRepositories.instance.updateTotalTimeQuality();
   }
 
   Future<void> _reestablishAll(BuildContext context) async {
     await _getIdCollectionAll(context);
     for (var item in _idAudioListAll) {
-      await _rep.copyPastCollections(
+      await CollectionsRepositories.instance.copyPastCollections(
         item,
         'DeleteCollections',
         'Collections',
       );
-      await _rep.deleteCollection(item, 'DeleteCollections');
-      await UserRepositories().updateTotalTimeQuality();
+      await CollectionsRepositories.instance.deleteCollection(
+        item,
+        'DeleteCollections',
+      );
+      await UserRepositories.instance.updateTotalTimeQuality();
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final state = context.watch<DeletePageModel>().getItemDone;
-    return PopupMenuButton(
-        icon: const Icon(
-          Icons.more_horiz,
-          color: AppColors.white,
-        ),
-        iconSize: 40,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(15),
+    return BlocBuilder<DeleteItemDoneCubit, bool>(
+      builder: (_, state) {
+        return PopupMenuButton(
+          icon: const Icon(
+            Icons.more_horiz,
+            color: AppColors.white,
           ),
-        ),
-        itemBuilder: state
-            ? (context) => [
-                  popupMenuItem(
-                    'Снять выделение',
-                    () => context.read<DeletePageModel>().stateCollections(),
-                  ),
-                  popupMenuItem(
-                    'Удалить',
-                    () => _delete(context),
-                  ),
-                  popupMenuItem(
-                    'Восстановить',
-                    () => _reestablish(context),
-                  ),
-                ]
-            : (context) => [
-                  popupMenuItem(
-                    'Выбрать несколько',
-                    () => context.read<DeletePageModel>().stateCollections(),
-                  ),
-                  popupMenuItem(
-                    'Удалить все',
-                    () => _deleteAll(context),
-                  ),
-                  popupMenuItem(
-                    'Восстановить все',
-                    () => _reestablishAll(context),
-                  ),
-                ]);
+          iconSize: 40.0,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(150),
+            ),
+          ),
+          itemBuilder: state
+              ? (context) => [
+                    popupMenuItem(
+                      'Снять выделение',
+                      () => context.read<DeleteItemDoneCubit>().itemDone(),
+                    ),
+                    popupMenuItem(
+                      'Удалить',
+                      () => _delete(context),
+                    ),
+                    popupMenuItem(
+                      'Восстановить',
+                      () => _reestablish(context),
+                    ),
+                  ]
+              : (context) => [
+                    popupMenuItem(
+                      'Выбрать несколько',
+                      () => context.read<DeleteItemDoneCubit>().itemDone(),
+                    ),
+                    popupMenuItem(
+                      'Удалить все',
+                      () => _deleteAll(context),
+                    ),
+                    popupMenuItem(
+                      'Восстановить все',
+                      () => _reestablishAll(context),
+                    ),
+                  ],
+        );
+      },
+    );
   }
 }
