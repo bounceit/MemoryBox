13e1d4f302a0a4d382ae71a4850107eb6dad13e6
Oleksandr <bounceit1221@gmail.com>
2022-09-08 13:23:12 +0300

    Update subscription_page.dart

--- lib/pages/drawer_pages/subscriptions_page/subscription_page.dart
+++ lib/pages/drawer_pages/subscriptions_page/subscription_page.dart
@@ -43,7 +43,7 @@ class SubscriptionPage extends StatelessWidget {
                     ),
                   ],
                 ),
-                SubscriptionAuthorisation()
+                // SubscriptionAuthorisation()
               ],
             ),
           ],
