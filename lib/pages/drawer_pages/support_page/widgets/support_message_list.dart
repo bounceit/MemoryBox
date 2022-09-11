e72445a22283c47cb634f126f981d15075561fc3
Oleksandr <bounceit1221@gmail.com>
2022-09-11 09:41:30 +0300

    Update support_message_list.dart

--- lib/pages/drawer_pages/support_page/widgets/support_message_list.dart
+++ lib/pages/drawer_pages/support_page/widgets/support_message_list.dart
@@ -1,65 +1,63 @@
-import 'package:audio_fairy_tales/recursec/app_colors.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:audio_fairy_tales/pages/drawer_pages/support_page/bloc/support_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../../repositories/user_repositories.dart';
-
-final UserRepositories rep = UserRepositories();
+import '../../../../repositories/auth_repository.dart';
 
 class SupportMessagePageList extends StatelessWidget {
   const SupportMessagePageList({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('SupportQuestions')
-            .orderBy('dateTime', descending: true)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Text('Error');
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (snapshot.hasData) {
-            final message = snapshot.data!;
-            return Expanded(
-              child: ListView(
-                padding: const EdgeInsets.all(15.0),
-                reverse: true,
-                children: message.docs.map((DocumentSnapshot document) {
-                  Map<String, dynamic> data =
-                      document.data() as Map<String, dynamic>;
-                  final message = data['message'] ?? '';
-                  final phoneNumber = data['phoneNumber'] ?? '';
-                  final currentUser = rep.user!.phoneNumber;
-                  bool? isMe;
-                  if (currentUser == phoneNumber) {
-                    isMe = true;
-                  }
-                  if ('+38000112233' == phoneNumber) {
-                    isMe = false;
-                  }
-                  return _ModelMessage(
-                      message: message,
-                      phoneNumber: phoneNumber,
-                      isMe: isMe ?? false);
-                }).toList(),
-              ),
-            );
-          }
+    return BlocBuilder<SupportMessageBloc, SupportMessageState>(
+      builder: (context, state) {
+        if (state.status == SupportMessageStatus.initial) {
           return const Center(
             child: CircularProgressIndicator(),
           );
-        });
+        }
+        if (state.status == SupportMessageStatus.success) {
+          return Expanded(
+            child: ListView.builder(
+                reverse: true,
+                padding: const EdgeInsets.all(15.0),
+                itemCount: state.list.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final message = state.list[index];
+                  final currentUser =
+                      AuthRepositories.instance.user!.phoneNumber;
+                  bool? isMe;
+                  if (currentUser == message.phoneNumber) {
+                    isMe = true;
+                  }
+                  if ('+38000112233' == message.phoneNumber) {
+                    isMe = false;
+                  }
+                  return ModelMessage(
+                      message: message.message,
+                      phoneNumber: message.phoneNumber,
+                      isMe: isMe ?? false);
+                }),
+          );
+        }
+        if (state.status == SupportMessageStatus.failed) {
+          return const Center(
+            child: Text('Ой: сталася помилка!'),
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
   }
 }
 
-class _ModelMessage extends StatelessWidget {
-  _ModelMessage({
+@immutable
+// ignore: must_be_immutable
+class ModelMessage extends StatelessWidget {
+  ModelMessage({
     Key? key,
     required this.message,
     required this.phoneNumber,
@@ -79,16 +77,20 @@ class _ModelMessage extends StatelessWidget {
           isMe! ? CrossAxisAlignment.end : CrossAxisAlignment.start,
       children: [
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10.0,
+          ),
           child: Text(
             phoneNumber!,
-            style: const TextStyle(fontSize: 12.0),
+            style: const TextStyle(
+              fontSize: 12.0,
+            ),
           ),
         ),
         Padding(
           padding: const EdgeInsets.all(5.0),
           child: Material(
-            color: isMe! ? AppColors.colorAppbar : Colors.grey.shade200,
+            color: isMe! ? Colors.blue.shade100 : Colors.grey.shade200,
             elevation: 3.0,
             borderRadius: isMe!
                 ? const BorderRadius.only(
@@ -105,7 +107,9 @@ class _ModelMessage extends StatelessWidget {
               padding: const EdgeInsets.all(10.0),
               child: Text(
                 message!,
-                style: const TextStyle(fontSize: 16.0),
+                style: const TextStyle(
+                  fontSize: 16.0,
+                ),
               ),
             ),
           ),
