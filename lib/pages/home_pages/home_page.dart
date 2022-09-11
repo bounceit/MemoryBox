407bca1a3510734d1e2296adeefa1ef7da0326ff
Oleksandr <bounceit1221@gmail.com>
2022-09-11 09:42:31 +0300

    Update home_page.dart

--- lib/pages/home_pages/home_page.dart
+++ lib/pages/home_pages/home_page.dart
@@ -1,68 +1,105 @@
+import 'dart:async';
+
 import 'package:audio_fairy_tales/pages/home_pages/appbar_home.dart';
-import 'package:audio_fairy_tales/recursec/app_colors.dart';
+import 'package:audio_fairy_tales/pages/home_pages/home_page_audio.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../../models/view_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../blocs/list_item_block/list_item_block.dart';
+import '../../repositories/audio_firebase_repositories.dart';
+import '../../repositories/auth_repository.dart';
 import '../../repositories/user_repositories.dart';
-import '../../widgets/main_page_widgets/audio_stack.dart';
-import '../main_pages/collections_pages/collections_repository.dart';
+import '../drawer_pages/subscriptions_page/subscription_page.dart';
+import 'bloc/blue_bloc/blue_bloc.dart';
+import 'bloc/green_bloc/green_bloc.dart';
+import 'bloc/orange_bloc/orange_bloc.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
   static const routeName = '/';
-
-  static Widget create() {
-    return const HomePage();
-  }
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final CollectionsRepositories repCollections = CollectionsRepositories();
-  final UserRepositories repUser = UserRepositories();
   Future<void> subscriptionDone(BuildContext context) async {
-    await FirebaseFirestore.instance
-        .collection(repCollections.user!.phoneNumber!)
-        .get()
-        .then((querySnapshot) {
-      for (var result in querySnapshot.docs) {
-        final bool subscription = result.data()['subscription'] ?? true;
-        if (subscription == false) {
-          Provider.of<Navigation>(context, listen: false).setCurrentIndex = 7;
-        }
-      }
-    });
+    if (AuthRepositories.instance.user != null) {
+      await FirebaseFirestore.instance
+          .collection(AuthRepositories.instance.user!.phoneNumber!)
+          .get()
+          .then(
+        (querySnapshot) {
+          for (var result in querySnapshot.docs) {
+            final bool subscription = result.data()['subscription'] ?? true;
+            if (subscription == false) {
+              Timer(const Duration(seconds: 3), () {
+                Navigator.pushNamed(
+                  context,
+                  SubscriptionPage.routeName,
+                );
+              });
+            }
+          }
+        },
+      );
+    }
   }
 
   @override
   void initState() {
-    // repUser.limitNotSubscription();
+    UserRepositories.instance.limitNotSubscription();
     subscriptionDone(context);
-    // AudioRepositories().finishDelete();
+    AudioRepositories.instance.finishDelete();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
-    return Scaffold(
-      backgroundColor: AppColors.white,
-      appBar: AppBar(
-        backgroundColor: AppColors.colorAppbar,
-        leading: IconButton(
-          onPressed: () {
-            Scaffold.of(context).openDrawer();
-          },
-          icon: const Icon(Icons.menu),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ListItemBloc>(
+          create: (context) => ListItemBloc()
+            ..add(
+              LoadListItemEvent(
+                sort: 'all',
+                collection: 'Collections',
+                nameSort: 'collections',
+              ),
+            ),
         ),
-        elevation: 0.0,
-      ),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: screenHeight * 0.89,
+        BlocProvider<GreenListItemBloc>(
+          create: (context) => GreenListItemBloc()
+            ..add(
+              LoadGreenListItemEvent(),
+            ),
+        ),
+        BlocProvider<OrangeListItemBloc>(
+          create: (context) => OrangeListItemBloc()
+            ..add(
+              LoadOrangeListItemEvent(),
+            ),
+        ),
+        BlocProvider<BlueListItemBloc>(
+          create: (context) => BlueListItemBloc()
+            ..add(
+              const LoadBlueListItemEvent(),
+            ),
+        ),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            icon: const Icon(Icons.menu),
+          ),
+          elevation: 0.0,
+        ),
+        body: SizedBox(
+          height: screenHeight - (kBottomNavigationBarHeight + 40.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: const [
@@ -70,7 +107,10 @@ class _HomePageState extends State<HomePage> {
                 flex: 8,
                 child: AppbarHeaderHomePage(),
               ),
-              Expanded(flex: 11, child: AudioStackWidget()),
+              Expanded(
+                flex: 9,
+                child: HomePageAudio(),
+              ),
             ],
           ),
         ),
