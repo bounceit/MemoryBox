42d0ab80075b02e45a017feb0f574a98fa382d48
Oleksandr <bounceit1221@gmail.com>
2022-09-12 13:49:52 +0300

    Create total_bloc.dart

---
+++ lib/pages/main_pages/all_audio_pages/bloc/bloc_total_qual/total_bloc.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+
+import 'package:audio_fairy_tales/pages/main_pages/all_audio_pages/bloc/bloc_total_qual/total_event.dart';
+import 'package:audio_fairy_tales/pages/main_pages/all_audio_pages/bloc/bloc_total_qual/total_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../repositories/user_repositories.dart';
+
+class QualityTotalTimeBloc
+    extends Bloc<QualityTotalTimeEvent, QualityTotalTimeState> {
+  QualityTotalTimeBloc()
+      : super(
+          const QualityTotalTimeState(),
+        ) {
+    on<LoadQualityTotalTimeEvent>((event, emit) {
+      try {
+        _qualityTotalTimeSubscription?.cancel();
+        _qualityTotalTimeSubscription =
+            UserRepositories.instance.readUser().listen((qualityTotalTime) {
+          add(
+            UpdateQualityTotalTimeEvent(qualityTotalTime: qualityTotalTime),
+          );
+        });
+      } on Exception {
+        emit(state.copyWith(
+          status: QualityTotalTimeStatus.failed,
+        ));
+      }
+    });
+
+    on<UpdateQualityTotalTimeEvent>((
+      UpdateQualityTotalTimeEvent event,
+      Emitter<QualityTotalTimeState> emit,
+    ) {
+      emit(
+        state.copyWith(
+          status: QualityTotalTimeStatus.success,
+          qualityTotalTime: event.qualityTotalTime,
+        ),
+      );
+    });
+  }
+  StreamSubscription? _qualityTotalTimeSubscription;
+  @override
+  Future<void> close() {
+    _qualityTotalTimeSubscription?.cancel();
+    return super.close();
+  }
+}
