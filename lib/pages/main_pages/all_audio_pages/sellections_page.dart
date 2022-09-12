857fbc8302d05a37d266331959e03e0903161b6d
Oleksandr <bounceit1221@gmail.com>
2022-09-12 13:50:27 +0300

    Create sellections_page.dart

---
+++ lib/pages/main_pages/all_audio_pages/sellections_page.dart
@@ -0,0 +1,98 @@
+import 'package:audio_fairy_tales/pages/main_pages/all_audio_pages/widgets/appbar_all_audio.dart';
+import 'package:audio_fairy_tales/pages/main_pages/all_audio_pages/widgets/list_all_audio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../blocs/list_item_block/list_item_block.dart';
+import '../../../utils/constants.dart';
+
+import 'bloc/bloc_anim/anim_bloc.dart';
+import 'bloc/bloc_total_qual/total_bloc.dart';
+import 'bloc/bloc_total_qual/total_event.dart';
+
+class SellectionsPage extends StatelessWidget {
+  const SellectionsPage({Key? key}) : super(key: key);
+  static const routeName = '/audio_recordings_page';
+  final bool shouldPop = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AnimBloc>(
+          create: (context) => AnimBloc(),
+        ),
+        BlocProvider<ListItemBloc>(
+          create: (context) => ListItemBloc()
+            ..add(
+              LoadListItemEvent(
+                sort: 'all',
+                collection: 'Collections',
+                nameSort: 'collections',
+              ),
+            ),
+        ),
+        BlocProvider<QualityTotalTimeBloc>(
+          create: (context) => QualityTotalTimeBloc()
+            ..add(
+              LoadQualityTotalTimeEvent(),
+            ),
+        ),
+      ],
+      child: WillPopScope(
+        onWillPop: () async {
+          return shouldPop;
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: const Icon(Icons.menu),
+            ),
+            elevation: 0.0,
+            title: const Text(
+              'Аудиозаписи',
+              style: oneTitleTextStyle,
+            ),
+          ),
+          body: Stack(
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Stack(
+                      children: const [
+                        ListPlayer(),
+                        AppbarHeaderAudioRecordings(),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              BlocBuilder<AnimBloc, AnimState>(
+                builder: (_, state) {
+                  return Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 5.0),
+                      child: Container(),
+                      // PlayerCollections(
+                      //   screenWight: screenWight,
+                      //   screenHeight: screenHeight,
+                      //   idCollection: 'all',
+                      //   animation: state.anim,
+                      // ),
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
