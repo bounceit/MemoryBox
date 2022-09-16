426daac5e6e47e849fae50c5646f4a377c9d20dd
Oleksandr <bounceit1221@gmail.com>
2022-09-16 09:24:27 +0300

    Create appbar_profile.dart

---
+++ lib/pages/main_pages/profile_pages/profile_page/widgets/appbar_profile.dart
@@ -0,0 +1,32 @@
+import 'package:audio_fairy_tales/custom_shape.dart';
+import 'package:audio_fairy_tales/recursec/app_colors.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../utils/constants.dart';
+
+class AppbarHeaderProfileProfile extends StatelessWidget {
+  const AppbarHeaderProfileProfile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipPath(
+          clipper: Customshape(),
+          child: Container(
+            color: AppColors.colorAppbar,
+            width: double.infinity,
+            height: 200.0,
+          ),
+        ),
+        const Align(
+          alignment: Alignment.topCenter,
+          child: Text(
+            'Твоя частичка',
+            style: twoTitleTextStyle,
+          ),
+        ),
+      ],
+    );
+  }
+}
