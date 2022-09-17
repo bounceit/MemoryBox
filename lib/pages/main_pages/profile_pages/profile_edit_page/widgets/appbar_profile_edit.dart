522f7a1162cf2ddc589250ebfd8368eeb6dbe04f
Oleksandr <bounceit1221@gmail.com>
2022-09-17 09:29:54 +0300

    Create appbar_profile_edit.dart

---
+++ lib/pages/main_pages/profile_pages/profile_edit_page/widgets/appbar_profile_edit.dart
@@ -0,0 +1,65 @@
+import 'package:audio_fairy_tales/custom_shape.dart';
+import 'package:audio_fairy_tales/recursec/app_colors.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../utils/constants.dart';
+import '../../../../../widgets/buttons/icon_back.dart';
+
+class AppbarHeaderProfileEdit extends StatelessWidget {
+  const AppbarHeaderProfileEdit({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(),
+        ClipPath(
+          clipper: Customshape(),
+          child: Container(
+            color: AppColors.colorAppbar,
+            width: double.infinity,
+            height: 280.0,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            top: 30.0,
+            left: 15.0,
+            right: 10.0,
+          ),
+          child: IconBack(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(
+            top: 30.0,
+            left: 10.0,
+            right: 10.0,
+          ),
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              'Профиль',
+              style: twoTitleTextStyle,
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(
+            top: 75.0,
+          ),
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              'Твоя частичка',
+              style: twoTitleTextStyle,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
