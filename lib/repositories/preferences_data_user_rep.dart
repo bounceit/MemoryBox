bb620b8669b73cf35f6e91df4b109f15ca486937
Oleksandr <bounceit1221@gmail.com>
2022-09-19 13:21:02 +0300

    Update preferences_data_user_rep.dart

--- lib/repositories/preferences_data_user_rep.dart
+++ lib/repositories/preferences_data_user_rep.dart
@@ -1,23 +1,41 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PreferencesDataUser {
+  PreferencesDataUser._();
+  static final PreferencesDataUser instance = PreferencesDataUser._();
+
   final _storage = SharedPreferences.getInstance();
 
   // Save data in SharedPreferences
 
-  Future<void> counter(String counter) async {
+  Future<void> saveImage(
+    String image,
+  ) async {
     SharedPreferences storageData = await _storage;
-    storageData.setString('counter_key', counter);
+    storageData.setString(
+      'image_key',
+      image,
+    );
   }
 
-  Future<void> saveName(String name) async {
+  Future<void> saveName(
+    String name,
+  ) async {
     SharedPreferences storageData = await _storage;
-    storageData.setString('name_key', name);
+    storageData.setString(
+      'name_key',
+      name,
+    );
   }
 
-  Future<void> saveNumber(String number) async {
+  Future<void> saveNumber(
+    String number,
+  ) async {
     SharedPreferences storageData = await _storage;
-    storageData.setString('number_key', number);
+    storageData.setString(
+      'number_key',
+      number,
+    );
   }
 
   Future<void> cleanKey() async {
