f7594b5e517cfff44dc7aacaa7116a7f13e28d75
Oleksandr <bounceit1221@gmail.com>
2022-09-19 13:21:30 +0300

    Update alert_dialog.dart

--- lib/widgets/buttons/alert_dialog.dart
+++ lib/widgets/buttons/alert_dialog.dart
@@ -3,22 +3,22 @@ import 'dart:async';
 import 'package:audio_fairy_tales/recursec/app_colors.dart';
 import 'package:audio_fairy_tales/repositories/user_repositories.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-import '../../pages/main_pages/collections_pages/collections_repository.dart';
 import '../../recursec/app_icons.dart';
+import '../../repositories/collections_repositories.dart';
 
 class AlertDialogApp {
-  AlertDialogApp({Key? key});
-  final CollectionsRepositories repositoriesCollection =
-      CollectionsRepositories();
+  AlertDialogApp._();
+  static final AlertDialogApp instance = AlertDialogApp._();
   void alertDone(BuildContext context) {
     showDialog<String>(
         context: context,
         builder: (BuildContext context) {
           Timer(const Duration(seconds: 1), () async {
-            await UserRepositories().updateTotalTimeQuality();
-            Navigator.pop(context);
+            await UserRepositories.instance.updateTotalTimeQuality();
           });
+          Navigator.pop(context);
           return AlertDialog(
             insetPadding: const EdgeInsets.all(75.0),
             shape: const RoundedRectangleBorder(
@@ -47,34 +47,46 @@ class AlertDialogApp {
         context: context,
         builder: (BuildContext context) => AlertDialog(
           shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+            borderRadius: BorderRadius.all(
+              Radius.circular(20.0),
+            ),
+          ),
           title: const Text(
             'Подтверждаете удаление?',
-            style: TextStyle(color: AppColors.pink, fontSize: 18.0),
+            style: TextStyle(
+              color: AppColors.pink,
+              fontSize: 18.0,
+            ),
           ),
           content: const Text(
             'Ваш файл перенесется в папку “Недавно удаленные”. Через 15 дней он исчезнет.',
             textAlign: TextAlign.center,
-            style: TextStyle(color: AppColors.colorText70, fontSize: 14.0),
+            style: TextStyle(
+              color: AppColors.colorText70,
+              fontSize: 14.0,
+            ),
           ),
           actions: <Widget>[
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+              ),
               child: TextButton(
                 onPressed: () async {
                   Navigator.pop(context);
                   alertDone(context);
-                  await repositoriesCollection.copyPastCollections(
+                  await CollectionsRepositories.instance.copyPastCollections(
                     idAudio,
                     fromCollection,
                     inCollection,
                   );
-                  await repositoriesCollection.deleteCollection(
-                      idAudio, fromCollection);
+                  await CollectionsRepositories.instance
+                      .deleteCollection(idAudio, fromCollection);
                 },
                 style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all(AppColors.colorAppbar),
+                  backgroundColor: MaterialStateProperty.all(
+                    AppColors.colorAppbar,
+                  ),
                   shape: MaterialStateProperty.all(
                     RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(50.0),
@@ -85,24 +97,31 @@ class AlertDialogApp {
                   ),
                 ),
                 child: const Padding(
-                  padding:
-                      EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
+                  padding: EdgeInsets.symmetric(
+                    vertical: 5.0,
+                    horizontal: 15.0,
+                  ),
                   child: Text(
                     'Да',
-                    style: TextStyle(color: Colors.white),
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(right: 50.0),
+              padding: const EdgeInsets.only(
+                right: 50.0,
+              ),
               child: TextButton(
                 onPressed: () {
                   Navigator.pop(context);
                 },
                 style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all(AppColors.white100),
+                  backgroundColor: MaterialStateProperty.all(
+                    AppColors.white100,
+                  ),
                   shape: MaterialStateProperty.all(
                     RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(50.0),
@@ -113,14 +132,68 @@ class AlertDialogApp {
                   ),
                 ),
                 child: const Padding(
-                  padding:
-                      EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                  padding: EdgeInsets.symmetric(
+                    vertical: 5.0,
+                    horizontal: 10.0,
+                  ),
                   child: Text(
                     'Нет',
-                    style: TextStyle(color: AppColors.colorText),
+                    style: TextStyle(
+                      color: AppColors.colorText,
+                    ),
                   ),
                 ),
               ),
+            ),
+          ],
+        ),
+      );
+
+  void alertDialogPermission(
+          BuildContext context, String text, IconData icon) =>
+      showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Icon(
+                  icon,
+                  size: 40.0,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(
+                width: 10.0,
+              ),
+              Flexible(
+                flex: 4,
+                child: Text(text),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: const Text(
+                'Отключить',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey,
+                ),
+              ),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              child: const Text(
+                'Настройки',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey,
+                ),
+              ),
+              onPressed: () => openAppSettings(),
             ),
           ],
         ),
