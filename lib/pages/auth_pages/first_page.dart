fe189dddafe3b16b14a5128fb2099b52c06e225a
Oleksandr <bounceit1221@gmail.com>
2022-09-22 20:10:02 +0300

    Update first_page.dart

--- lib/pages/auth_pages/first_page.dart
+++ lib/pages/auth_pages/first_page.dart
@@ -23,48 +23,50 @@ class FirstPage extends StatelessWidget {
               subtitle: 'Твой голос всегда рядом',
             ),
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  bottom: 100.0,
-                ),
-                child: SizedBox(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Привет!',
-                        style: TextStyle(
-                          fontSize: 24.0,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20.0,
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 55.0,
-                        ),
-                        child: Text(
-                          'Мы рады видеть тебя здесь. Это приложение поможет записывать сказки и держать их в удобном месте не заполняя память на телефоне.',
-                          textAlign: TextAlign.center,
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 100.0,
+                  ),
+                  child: SizedBox(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Привет!',
                           style: TextStyle(
-                            fontSize: 16.0,
+                            fontSize: 24.0,
                           ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 30.0,
-                      ),
-                      ButtonContinue(
-                        onPressed: () {
-                          Navigator.pushNamed(
-                            context,
-                            RegistrationPage.routeName,
-                          );
-                        },
-                        text: 'Продолжыть',
-                      )
-                    ],
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 55.0,
+                          ),
+                          child: Text(
+                            'Мы рады видеть тебя здесь. Это приложение поможет записывать сказки и держать их в удобном месте не заполняя память на телефоне.',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30.0,
+                        ),
+                        ButtonContinue(
+                          onPressed: () {
+                            Navigator.pushNamed(
+                              context,
+                              RegistrationPage.routeName,
+                            );
+                          },
+                          text: 'Продолжыть',
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
